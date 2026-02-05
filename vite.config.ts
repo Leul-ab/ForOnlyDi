1c5b87a49e8e076836fb70c1358edbba0dbf818f
Leul-ab <leulabera321@gmail.com>
2026-02-05 17:19:19 +0300

    Intial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const isGHPages = process.env.NODE_ENV === "production";
 export default defineConfig({
   plugins: [react()],
-  base: "/Di-s-Gift/",
+  base: isGHPages ? "/ForDi/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
 });
+
 
