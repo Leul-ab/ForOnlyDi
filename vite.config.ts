8a6902b427d0305ae9624473e76a6f21f6754a51
Leul-ab <leulabera321@gmail.com>
2026-02-05 14:03:55 +0300

    Fix base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Di-s-Gift",
+  base: "/Di-s-Gift/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
