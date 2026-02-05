c72bf97b09afb76f0797638520e6cf21d4dc297d
Leul-ab <leulabera321@gmail.com>
2026-02-05 18:21:21 +0300

    Update homepage layout

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-const isGHPages = process.env.NODE_ENV === "production";
+//const isGHPages = process.env.NODE_ENV === "production";
 export default defineConfig({
   plugins: [react()],
-  base: isGHPages ? "/ForDi/" : "/",
+  base:  "/ForOnlyDi/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
